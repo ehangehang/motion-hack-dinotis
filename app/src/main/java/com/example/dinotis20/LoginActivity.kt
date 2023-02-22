3642452c4d6c08613adfbd36f948d6d73121851e
riezaf51 <riezaf51@gmail.com>
2023-02-22 19:07:25 +0700

    tambah login dan splash activity

---
+++ app/src/main/java/com/example/dinotis20/LoginActivity.kt
@@ -0,0 +1,27 @@
+package com.example.dinotis20
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import com.google.firebase.auth.FirebaseAuth
+
+class LoginActivity : AppCompatActivity() {
+    // Firebase
+    private lateinit var auth: FirebaseAuth
+
+    // variables
+    private var phoneNumber = "+62"
+
+    // elements
+    private val edtPhoneNumber = findViewById<EditText>(R.id.lg_edt_phonenum)
+    private val edtPassword = findViewById<EditText>(R.id.lg_edt_pass)
+    private val btLogin = findViewById<Button>(R.id.lg_bt_login)
+    private val txtRegister = findViewById<TextView>(R.id.lg_txt_register)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
