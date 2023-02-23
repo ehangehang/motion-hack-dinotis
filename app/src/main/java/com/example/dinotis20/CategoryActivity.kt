8aa058f2ec48d3cc769c7675c455ca0e71dae167
ehangehang <rayhansuryatama@gmail.com>
2023-02-24 01:07:13 +0700

    UI implementation home categories

--- app/src/main/java/com/example/dinotis20/CategoryActivity.kt
+++ app/src/main/java/com/example/dinotis20/CategoryActivity.kt
@@ -2,10 +2,24 @@ package com.example.dinotis20
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class CategoryActivity : AppCompatActivity() {
+
+    private lateinit var btBack: ImageView
+
+    private fun init() {
+        btBack = findViewById(R.id.category_bt_back)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_category)
+
+        init()
+
+        btBack.setOnClickListener{
+            finish()
+        }
     }
 }
--- app/src/main/java/com/example/dinotis20/ScheduleActivity.kt
+++ app/src/main/java/com/example/dinotis20/ScheduleActivity.kt
@@ -2,10 +2,25 @@ package com.example.dinotis20
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 
 class ScheduleActivity : AppCompatActivity() {
+
+    private lateinit var btBack: ImageView
+
+    private fun init() {
+        btBack = findViewById(R.id.schedule_bt_back)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_schedule)
+
+        init()
+
+        btBack.setOnClickListener {
+            finish()
+        }
     }
 }
